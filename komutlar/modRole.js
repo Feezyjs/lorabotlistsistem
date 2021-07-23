7d7428270bfca9773536cd012cb18c9dea2a6fb1
Feezy <82213751+Feezyjs@users.noreply.github.com>
2021-07-23 20:52:06 +0200

    Update modRole.js

--- komutlar/modRole.js
+++ komutlar/modRole.js
@@ -6,7 +6,7 @@ exports.run = async(client, message, args) => {
 let mentionRole = message.mentions.roles.first();
   db.set(`modRole.${message.guild.id}`, mentionRole.id);
  message.channel.send(new Discord.MessageEmbed()
-  .setAuthor("EtherZ Bot",client.user.avatarURL())
+  .setAuthor("feezy",client.user.avatarURL())
   .setColor("#f8f8f9")
   .setDescription(`**<a:tenor:807201675738611800> | Mod rol başarıyla ${mentionRole} olarak ayarlandı**`)
   .setFooter("Tüm Hakları Saklıdır."))
